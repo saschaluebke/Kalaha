88acbb6c42183517956d2f87f02f7a56f5f6dd07
saschaluebke <sascha.luebke@web.de>
2016-12-08 16:05:58 +0100

    1

---
+++ src/Testen.java
@@ -0,0 +1,11 @@
+import info.kwarc.teaching.AI.Kalah.Game;
+import info.kwarc.teaching.AI.Kalah.RandomPlayer;
+
+public class Testen {
+
+	public static void main(String[] args) {
+		Game g = new Game(new DerDude(), new RandomPlayer("p2"), 6, 6);
+		System.out.println(g.play(true));
+	}
+
+}
