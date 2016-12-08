2cfeb944a2bad2eba5f2878d72f49391b7075727
saschaluebke <sascha.luebke@web.de>
2016-12-08 16:05:30 +0100

    1

--- src/DerDude.java
+++ src/DerDude.java
@@ -1,4 +1,29 @@
+import info.kwarc.teaching.AI.Kalah.Board;
 
-public class DerDude {
+public class DerDude extends info.kwarc.teaching.AI.Kalah.Agent{
+
+	@Override
+	public void init(Board board, boolean playerOne) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int move() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<String> students() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
